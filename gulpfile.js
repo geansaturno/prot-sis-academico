d10d3b888359d8fd7249cb2b27fcde2db7d53914
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2017-07-12 00:50:49 -0300

    final do prototipo

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,38 @@ let less = require("gulp-less");
 let clean = require("gulp-clean");
 
 
-gulp.task('clean-standalone', function() {
+gulp.task('clean-standalone', function () {
     return gulp.src('css/standalone')
-    .pipe(clean());
+        .pipe(clean());
 });
 
 gulp.task('less', () => {
-   gulp.src('less/bootstrap/bootstrap.less')
-   .pipe(less())
-     .pipe(gulp.dest('css'));
+    gulp.src('less/bootstrap/bootstrap.less')
+        .pipe(less())
+        .pipe(gulp.dest('css'));
 });
 
-gulp.task('less-standalone', ["clean-standalone"], function() {
+gulp.task('less-standalone', ["clean-standalone"], function () {
     gulp.src('less/standalone/**/*.less')
-    .pipe(less())
-      .pipe(gulp.dest('css/standalone'));
+        .pipe(less())
+        .pipe(gulp.dest('css/standalone'));
 });
 
 gulp.task('default', ['less', 'less-standalone'], () => {
     gulp.watch('less/bootstrap/**/*.less', ['less']);
     gulp.watch('less/standalone/**/*.less', ['less-standalone']);
+});
+
+gulp.task('clean-build', function () {
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build', ["clean-build"], function () {
+    gulp.src('*.html')
+        .pipe(gulp.dest('dist'));
+    gulp.src('css/**/*')
+        .pipe(gulp.dest('dist/css/'));
+    gulp.src('js/**/*')
+        .pipe(gulp.dest('dist/js'));
 });
